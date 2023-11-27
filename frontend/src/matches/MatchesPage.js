55cafe9151cd226782e25f799850ed68e634cce3
riyagori1203 <rvg7437@gmail.com>
2023-11-26 19:49:01 -0500

    feature to set preference on login

--- frontend/src/matches/MatchesPage.js
+++ frontend/src/matches/MatchesPage.js
@@ -4,20 +4,7 @@ const Recommendations = () => {
   const [recommendedJobs, setRecommendedJobs] = useState([]);
 
   useEffect(() => {
-    const checkFieldsAndFetch = async () => {
-      const skills = localStorage.getItem("skills");
-      const experienceLevel = localStorage.getItem("experienceLevel");
-      const location = localStorage.getItem("location");
-
-      if (!skills || !experienceLevel || !location) {
-        alert("Please fill in all required fields in the profile page for accurate matches.");
-        return;
-      }
-
-      await fetchRecommendations();
-    };
-
-    checkFieldsAndFetch();
+    fetchRecommendations();
   }, []);
 
   const fetchRecommendations = async () => {
@@ -40,7 +27,94 @@ const Recommendations = () => {
 
   return (
     <div>
-      {/* ... (existing code) */}
+      <h2 class="d-flex justify-content-center my-5">Recommended Jobs</h2>
+      <table
+        classname="table my-4"
+        style={{
+          boxShadow: "0px 5px 12px 0px rgba(0,0,0,0.1)",
+          marginTop: 30,
+          marginLeft: "10%",
+        }}
+      >
+        <thead>
+          <tr>
+            <th
+              className="p-3"
+              style={{
+                fontSize: 18,
+                fontWeight: "500",
+                backgroundColor: "#2a6e85",
+                color: "#fff",
+              }}
+            >
+              Company Name
+            </th>
+            <th
+              className="p-3"
+              style={{
+                fontSize: 18,
+                fontWeight: "500",
+                backgroundColor: "#2a6e85",
+                color: "#fff",
+              }}
+            >
+              Job Title
+            </th>
+            <th
+              className="p-3"
+              style={{
+                fontSize: 18,
+                fontWeight: "500",
+                backgroundColor: "#2a6e85",
+                color: "#fff",
+              }}
+            >
+              Link
+            </th>
+            <th
+              className="p-3"
+              style={{
+                fontSize: 18,
+                fontWeight: "500",
+                backgroundColor: "#2a6e85",
+                color: "#fff",
+              }}
+            >
+              Location
+            </th>
+            {/* Add more fields as needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {console.log(recommendedJobs)}
+          {recommendedJobs.map((job, index) => (
+            <tr key={index}>
+              <td className="p-3">{job.companyName}</td>
+              <td className="p-3">{job.jobTitle}</td>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={job.data_share_url}
+              >
+                <button
+                  type="button"
+                  class="btn btn-primary d-flex align-items-center"
+                  style={{
+                    backgroundColor: "#2a6e85",
+                    margin: "5px",
+                    width: "100px",
+                    verticalAlign: "middle",
+                  }}
+                >
+                  Job Link
+                </button>
+              </a>
+              {/* <a className='p-3' href='{job.data_share_url}'>Job Link</a> */}
+              <td className="p-3">{job.location}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
