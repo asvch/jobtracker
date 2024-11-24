9df90e664139694f7b52137ca5699f3dd7910863
Ruthwik Hosadurga Madhusudhana <ruthwik@Ruthwiks-MacBook-Air-2.local>
2024-11-24 15:38:38 -0500

    Fix import issue with ModalHeader

--- frontend/src/profile/CustomProfileModal.js
+++ frontend/src/profile/CustomProfileModal.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { Modal, ModalBody, ModalFooter, Form, Button } from 'react-bootstrap';
-import ModalHeader from 'react-bootstrap/esm/ModalHeader';
 import { CONSTANTS } from '../data/Constants';
 import { baseApiURL } from '../api/base.ts';
 
@@ -65,7 +64,7 @@ const CustomProfileModal = (props) => {
 					{
 						headers: {
 							userid: profile.id,
-							Authorization: `Bearer ${localStorage.getItem('userId')}`
+							Authorization: `Bearer ${localStorage.getItem('token')}`
 						}
 					}
 				);
@@ -89,10 +88,10 @@ const CustomProfileModal = (props) => {
 
 	return (
 		<Modal show={true} centered>
-			<ModalHeader>
+			<Modal.Header>
 				<h5 class='modal-title'>Edit Details</h5>
 				<button type='button' class='btn-close' aria-label='Close' onClick={() => setModalOpen(false)}></button>
-			</ModalHeader>
+			</Modal.Header>
 			<ModalBody>
 				{error && <div style={{ color: 'red', fontSize: 12, marginBottom: 10 }}>{errorMessage}</div>}
 				<Form>
