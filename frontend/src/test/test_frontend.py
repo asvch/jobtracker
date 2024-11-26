e9a8d7856c62ec15e1360f56583e48fcdc39f01c
Ajay Chundi <66492476+asvch@users.noreply.github.com>
2024-11-26 13:39:17 -0500

    Update test_frontend.py

--- frontend/src/test/test_frontend.py
+++ frontend/src/test/test_frontend.py
@@ -8,9 +8,9 @@ import time
 @pytest.fixture(scope="module")
 def driver():
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=chrome_options)
     # driver = webdriver.Chrome()
     driver.get("http://localhost:3000")  
