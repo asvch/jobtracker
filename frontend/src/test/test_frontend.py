53600bf574fa53f1125017ed4a67a3017ced914c
Ajay Chundi <66492476+asvch@users.noreply.github.com>
2024-11-26 13:56:41 -0500

    Update test_frontend.py

--- frontend/src/test/test_frontend.py
+++ frontend/src/test/test_frontend.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import pytest
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture(scope="module")
 def driver():
@@ -27,21 +29,21 @@ def test_login_page(driver):
 
     print("Text 'Job Tracker' is displayed on the page")
 
-# def test_invalid_login(driver):
-#     username_field = driver.find_element(By.ID, "uname")
-#     password_field = driver.find_element(By.ID, "pwd")
-#     login_button = driver.find_element(By.XPATH, "/html/body/div/div/div/div/div[2]/div/div/div[1]/form/div[3]/button[1]")
+def test_invalid_login(driver):
+    username_field = driver.find_element(By.ID, "uname")
+    password_field = driver.find_element(By.ID, "pwd")
+    login_button = driver.find_element(By.XPATH, "/html/body/div/div/div/div/div[2]/div/div/div[1]/form/div[3]/button[1]")
 
-#     username_field.send_keys("sponge9")   # invalid login and pword
-#     password_field.send_keys("sponge9")
-#     login_button.click()
+    username_field.send_keys("sponge9")   # invalid login and pword
+    password_field.send_keys("sponge9")
+    login_button.click()
 
-#     time.sleep(5)
+    time.sleep(5)
 
-#     alert = driver.switch_to.alert
-#     alert_text = alert.text
-#     alert.accept()
-#     assert "Error while login" in alert_text, "Alert message is not as expected"
+    alert = driver.switch_to.alert
+    alert_text = alert.text
+    alert.accept()
+    assert "Error while login" in alert_text, "Alert message is not as expected"
 
 
 def test_signup(driver):
@@ -50,13 +52,19 @@ def test_signup(driver):
     fullname = driver.find_element(By.ID, "fullname")
     username_field = driver.find_element(By.ID, "suname")
     password_field = driver.find_element(By.ID, "spwd")
-    signup_button = driver.find_element(By.XPATH, "/html/body/div/div/div/div/div[2]/div/div/div[2]/form/div[4]/button[1]")
+    # signup_button = driver.find_element(By.XPATH, "/html/body/div/div/div/div/div[2]/div/div/div[2]/form/div[4]/button[1]")
 
     signup_tab.click()
     time.sleep(3)
+    signup_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//button[contains(text(),"Sign Up")]'))
+    )
+    # driver.save_screenshot("screenshot.png")  # Save a screenshot for debugging
     fullname.send_keys("sponge10")
     username_field.send_keys("sponge10")
     password_field.send_keys("sponge10")
+    # signup_button.click()
+    driver.execute_script("arguments[0].scrollIntoView(true);", signup_button)
     signup_button.click()
 
     time.sleep(5)
